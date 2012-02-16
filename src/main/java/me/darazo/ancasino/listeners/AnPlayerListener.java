7f5372f6d0bebc8a6a018747adf70fb2bb17d5f3
Max D <maxdeutsch@gmx.de>
2012-02-16 23:18:26 +0100

    Update src/main/java/me/darazo/ancasino/listeners/AnPlayerListener.java

--- src/main/java/me/darazo/ancasino/listeners/AnPlayerListener.java
+++ src/main/java/me/darazo/ancasino/listeners/AnPlayerListener.java
@@ -31,7 +31,7 @@ public class AnPlayerListener implements Listener {
 			Block b = event.getClickedBlock();
 				
 			// Left clicked note block
-			if(b.getType() == Material.NOTE_BLOCK) {
+			if(event.hasBlock() && b.getType().equals(Material.NOTE_BLOCK)) {
 					
 				// Look for matching controller block
 				for(SlotMachine slot : plugin.slotData.getSlots()) {
